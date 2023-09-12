4192ddac01b3ee982df02522288e72d1bf90a4bc
YactayoC <syactayocrisostomo@gmail.com>
2023-09-12 12:45:50 -0500

    add user model in database

--- src/models/user.py
+++ src/models/user.py
@@ -5,14 +5,34 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
+    date_of_birth = db.Column(db.Date)
+    cellphone = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(
+        self,
+        email,
+        password,
+        name,
+        lastname,
+        date_of_birth,
+        cellphone,
+    ):
+        self.email = email
         self.password = password
+        self.name = name
+        self.lastname = lastname
+        self.date_of_birth = date_of_birth
+        self.cellphone = cellphone
 
     def to_dict(self):
         return {
-            "username": self.username,
+            "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "date_of_birth": self.date_of_birth,
+            "cellphone": self.cellphone,
         }

883d67f6d0a089b43568958390e25ca61e1ec432
YactayoC <syactayocrisostomo@gmail.com>
2023-09-12 12:46:25 -0500

    add endpoints for user authentication

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -2,40 +2,42 @@ from flask import Blueprint, request, jsonify
 from src.models.user import User
 from src.services.auth_service import AuthService
 from src.database.db_pg import db
+from src.middleware.token_required import token_required
 
 
 auth_bp = Blueprint("auth", __name__)
 
 
-@auth_bp.route("/sigin", methods=["POST"])
-def sigin():
-    # Obtiene los datos del usuario desde el body del request
-    username = request.json.get("username")
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email")
     password = request.json.get("password")
 
-    # Si el usuario o la contraseña no existen, devuelve un error
-    if not username or not password:
+    if not email or not password:
         return jsonify({"error": "Usuario y contraseña requeridos"}), 400
 
-    # Registra al usuario
-    response, status = AuthService.login_user(username, password)
-
-    # Devuelve la respuesta y el código de estado HTTP
+    response, status = AuthService.login_user(email, password)
     return jsonify(response), status
 
 
-@auth_bp.route("/sigup", methods=["POST"])
-def sigup():
-    # Obtiene los datos del usuario desde el body del request
-    username = request.json.get("username")
-    password = request.json.get("password")
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    try:
+        user_data = request.get_json()
 
-    # Si el usuario o la contraseña no existen, devuelve un error
-    if not username or not password:
-        return jsonify({"error": "Usuario y contraseña requeridos"}), 400
+        required_fields = [
+            "name",
+            "lastname",
+            "date_of_birth",
+            "cellphone",
+            "email",
+            "password",
+        ]
+        for field in required_fields:
+            if field not in user_data:
+                return jsonify({"error": f"El campo '{field}' es requerido."}), 400
 
-    # Registra al usuario
-    response, status = AuthService.register_user(username, password)
-
-    # Devuelve la respuesta y el código de estado HTTP
-    return jsonify(response), status
+        response, status = AuthService.register_user(user_data)
+        return jsonify(response), status
+    except Exception as e:
+        return {"error": str(e)}, 500

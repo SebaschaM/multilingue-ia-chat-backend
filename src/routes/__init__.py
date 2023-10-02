e600e4bccb72186090d4cd2ba79e2e230b8defc2
YactayoC <syactayocrisostomo@gmail.com>
2023-10-01 20:56:41 -0500

    add route request client

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,7 +1,16 @@
 # import all routes admin
 from src.routes.admin.auth_admin import auth_admin_bp
 
+# Prefix for all routes admin
+auth_admin_bp.url_prefix = "/api/admin/auth"
+
 # import all routes client
 from src.routes.client.auth_client import auth_client_bp
+from src.routes.client.request_client import request_client_bp
 
-blueprints = [auth_admin_bp, auth_client_bp]
+# Prefix for all routes client
+auth_client_bp.url_prefix = "/api/client/auth"
+request_client_bp.url_prefix = "/api/client/request"
+
+
+blueprints = [auth_admin_bp, auth_client_bp, request_client_bp]

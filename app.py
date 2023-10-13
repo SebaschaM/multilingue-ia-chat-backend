b48a4d7ae3e5014f196dfcb44ed03c1c1e41fac8
YactayoC <syactayocrisostomo@gmail.com>
2023-10-13 16:16:05 -0500

    add user blocking scheduling and request rate limiting

--- app.py
+++ app.py
@@ -1,44 +1,54 @@
-from flask import Flask
-from dotenv import load_dotenv
+from flask import Flask, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_socketio import SocketIO
 from flask_cors import CORS
-import logging
+from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
 import os
 
 from src.database.db_pg import db
 from src.utils.send_mail import configure_mail
-
-# Import socket io
-from flask_socketio import SocketIO
+from src.utils.schedules import Schedules
 from src.routes import blueprints
 
 load_dotenv()
-cors_allowed_origins = "*"
+socketio = SocketIO(cors_allowed_origins="*")
 
-socketio = SocketIO(cors_allowed_origins=cors_allowed_origins)
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+socketio.init_app(app)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["15 per minute", "5 per second"],
+    storage_uri="memory://",  # De prueba, cambiar a redis
+    strategy="fixed-window",
+)
 
 
 def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    socketio.init_app(app)
-
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
 
     mail = configure_mail(app)
+    programador = BackgroundScheduler(daemon=True)
+    programador.add_job(Schedules.block_users, "interval", seconds=10, args=(app, db))
+    programador.start()
     return app
 
 
-# !Esto no deberia estar aqui
-if __name__ == "__main__":
-    # Inicializa SocketIO solo si el archivo se ejecuta directamente
-    socketio.run(create_app(), debug=True)
-from src.sockets.socketios_event import (
-    handle_connect,
-    handle_login,
-    handle_create_private_room,
-    handle_join_private_room,
-)
+# # !Esto no deberia estar aqui
+# if __name__ == "__main__":
+#     # Inicializa SocketIO solo si el archivo se ejecuta directamente
+#     socketio.run(create_app(), debug=True)
+# from src.sockets.socketios_event import (
+#     handle_connect,
+#     handle_login,
+#     handle_create_private_room,
+#     handle_join_private_room,
+# )
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 from sqlalchemy import text
 
+
 from src.database.db_pg import db
 from src.models.seeds import create_seed
 
@@ -10,5 +11,6 @@ with app.app_context():
     db.create_all()
     create_seed()
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=False, port=5000, host="0.0.0.0")

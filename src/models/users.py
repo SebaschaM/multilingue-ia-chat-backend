569dec385fe4c5ccdbefcd612da53157a2b02e23
YactayoC <syactayocrisostomo@gmail.com>
2023-11-13 11:38:13 -0500

    fix: register user duplicate uuid

--- src/models/users.py
+++ src/models/users.py
@@ -10,7 +10,7 @@ class Users(db.Model):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    uuid = Column(String, nullable=False, default=str(uuid.uuid4()), unique=True)
+    uuid = Column(String, nullable=False, unique=True)
     fullname = Column(String(80))
     cellphone = Column(String(80))
     email = Column(String(80), unique=True, nullable=False)
@@ -63,6 +63,7 @@ class Users(db.Model):
         blocked=None,
         created_at=None,
     ):
+        self.uuid = str(uuid.uuid4())
         self.email = email
         self.password = password
         self.fullname = fullname

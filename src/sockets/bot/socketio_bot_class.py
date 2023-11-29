2a7ede032049eca0d0c9281d79a521d2e7e944f3
YactayoC <syactayocrisostomo@gmail.com>
2023-11-29 13:25:28 -0500

    fix table conversations

--- src/sockets/bot/socketio_bot_class.py
+++ src/sockets/bot/socketio_bot_class.py
@@ -7,7 +7,7 @@ from src.services.common.message_service import MessageService
 
 
 class MessageBotHandler:
-    api_key = "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"  #! No subirlo
+    api_key = ""  #! No subirlo
     context_file = "context.txt"
     context = ""
     context_initialized = False
